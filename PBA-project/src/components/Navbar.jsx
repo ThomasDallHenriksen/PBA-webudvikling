aa4905912c4262708597af3ebab31a0caaa2e723
Thomas Henriksen <thomas-henriksen12@hotmail.com>
2023-11-10 13:32:59 +0100

    working

--- PBA-project/src/components/Navbar.jsx
+++ PBA-project/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 // import {Link} from 'react-router-dom';
-import '../index.css'
+import '../assets/styles/navbar.scss';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="link">
                 <a href='/'>Home</a>
-                <a href='/Create'>Create</a>
+                <a href='/Products'>Products</a>
+                <a href='/Company'>Company</a>
+                <a href='/Contact'>Contact</a>
+                <a id='airMap' href='/AirMap'>AirMap</a>
             </div>
         </nav>
     );

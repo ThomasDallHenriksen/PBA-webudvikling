9a9901ccdfab3216cb328bf51df4ef6af7ab7f8f
“Simon <sikje96@gmail.com>
2023-11-13 14:42:12 +0100

    Tilføjet listen til footer

---
+++ PBA-project/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import '../assets/styles/footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="block">
+      <div className="block__container">
+        <div className="block__home">HOME</div>
+        <div className="block__product">PRODUCT</div>
+        <div className="block__company">COMPANY</div>
+        <div className="block__contact">CONTACT</div>
+        <div className="block__AirMaps">AirMaps</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- PBA-project/src/pages/Home.jsx
+++ PBA-project/src/pages/Home.jsx
@@ -1,26 +1,15 @@
 import '../assets/global/breakpoints.scss'
 import '../assets/main/font.scss'
+import Footer from '../components/Footer';
 import '../index.css'
 
 const Home = () => {
     return (
         <div className="home">
             <h2>Home</h2>
+            <Footer />
         </div>
     );
 }
 
 export default Home;
-//  <style lang="scss" scoped>
-// .home {
-//     background-color: red;
-
-//         @include media("xs") {
-//             background-color: var(--color-skyblue);
-//         }   
-
-//         @include media("sm") {
-//             background-color: var(--color-black);
-//         }   
-// }
-// </style> 

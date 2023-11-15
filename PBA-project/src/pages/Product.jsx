750e043c52f2825e6cd9b9266bfd43925ab74d30
Thomas Henriksen <thomas-henriksen12@hotmail.com>
2023-11-15 14:16:41 +0100

    added caousel

--- PBA-project/src/pages/Product.jsx
+++ PBA-project/src/pages/Product.jsx
@@ -1,13 +1,24 @@
+import React from 'react';
 import '../assets/global/breakpoints.scss'
 import '../assets/main/font.scss'
 import '../index.css'
+import Carousel from '../components/Carousel'
 
 const Product = () => {
+    <div className="product-section-1"></div>
+    const images = [
+      'images/carousel/carousel1.png',
+      'images/carousel/carousel2.png',
+      'images/carousel/carousel3.png',
+      // Add more image URLs as needed
+    ];
+  
     return (
-        <div className="product">
-            <h2>Product</h2>
-        </div>
+      <div>
+        <h1>Your Product</h1>
+        <Carousel images={images} />
+      </div>
     );
-}
-
-export default Product;
+  };
+  
+  export default Product;

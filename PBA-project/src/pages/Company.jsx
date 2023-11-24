264aaaf6bb8b921cde52a9900e46f03d00895652
Thomas Henriksen <thomas-henriksen12@hotmail.com>
2023-11-24 11:50:30 +0100

    Company page done - also added a xxs breakpoint with 540px

--- PBA-project/src/pages/Company.jsx
+++ PBA-project/src/pages/Company.jsx
@@ -28,7 +28,7 @@ const Company = () => {
                 </div>
                 <div className="our-succes-story">
                     <h4>Our succes story</h4>
-                    <h2>Lorem Ipsum</h2>
+                    <p id='bigger-p'>Lorem Ipsum</p>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
             </div>

7515b9a36f1a2e8666e8f38fc22d9f167b26e726
Kim Le <lekim@live.dk>
2023-12-15 10:17:43 +0100

    db.con update

--- PBA-project/src/pages/Addairplate.jsx
+++ PBA-project/src/pages/Addairplate.jsx
@@ -1,8 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
 import '../assets/styles/add.scss';
 import { Link } from 'react-router-dom';
 
 
 const Addairplate = () => {
+    const [serialNumber, setSerialNumber] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const user = sessionStorage.getItem('user');
+        setIsLoggedIn(!!user);
+    }, []);
+
+    const handleAdd = async (e) => {
+        e.preventDefault();
+
+        if (!isLoggedIn) {
+            alert('you must logged in to add a Airplate');
+            return;
+        }
+
+        try {
+            const response = await axios.post('https://kienzhe.dk/backend/addAirplate.php', {
+                serialNumber: serialNumber,
+            });
+
+            const data = response.data;
+
+            if (data.status === 'succes') {
+                window.location.href = '/Adddrone';
+            } else {
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
     return (
         <div className="block">
             <div className="top">
@@ -14,9 +49,11 @@ const Addairplate = () => {
                     type="text"
                     className="serial-input"
                     placeholder="Serial number..."
+                    value={serialNumber}
+                    onChange={(e) => setSerialNumber(e.target.value)}
                 />
                 <Link to="/Adddrone">
-                    <button className="next-button">Next</button>
+                    <button className="next-button" onClick={handleAdd}>Next</button>
                 </Link>
             </div>
         </div>
